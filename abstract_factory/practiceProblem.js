bceae611b5a9b2f94e0de25568049fb85d90761f
annaclaratxm <annaclaratxm@gmail.com>
2025-09-04 17:15:46 -0300

    abstract factory examples

--- abstract_factory/practiceProblem.js
+++ abstract_factory/practiceProblem.js
@@ -43,3 +43,66 @@ function main(format) {
 
 main("pdf");
 main("docx");
+
+// Abstract Factory
+class DocumentFactory {
+  createReport() {
+    throw new Error("Método abstrato deve ser implementado");
+  }
+  createInvoice() {
+    throw new Error("Método abstrato deve ser implementado");
+  }
+}
+
+// Concrete Factories
+class PdfFactory extends DocumentFactory {
+  createReport() {
+    return new PdfReport();
+  }
+  createInvoice() {
+    return new PdfInvoice();
+  }
+}
+
+class DocxFactory extends DocumentFactory {
+  createReport() {
+    return new DocxReport();
+  }
+  createInvoice() {
+    return new DocxInvoice();
+  }
+}
+
+// Cliente
+const factories = {
+  pdf: new PdfFactory(),
+  docx: new DocxFactory(),
+};
+
+class DocumentApp {
+  constructor(factory) {
+    this.report = factory.createReport();
+    this.invoice = factory.createInvoice();
+  }
+
+  generateDocuments() {
+    console.log(this.report.generate());
+    console.log(this.invoice.generate());
+  }
+}
+
+// Uso
+function main() {
+  const types = ["pdf", "docx"];
+
+  types.forEach((type) => {
+    console.log(`\n>> Fábrica selecionada: ${type} <<`);
+    const factory = factories[type];
+    if (!factory) throw new Error("Tipo de fábrica desconhecido");
+
+    const app = new DocumentApp(factory);
+    app.generateDocuments();
+  });
+}
+
+main();
