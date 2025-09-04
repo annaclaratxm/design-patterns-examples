deed61aa6bfbc163913fb2874cedc89d42bd9868
annaclaratxm <annaclaratxm@gmail.com>
2025-09-04 18:25:32 -0300

    fix: examples factoty method

--- factory_method/index.js
+++ factory_method/index.js
@@ -1,56 +1,58 @@
 // Classes concretas
-class DebitCard {
-  constructor(amount) {
-    this.amount = amount; // o amount fica armazenado na instância
-  }
-
-  pay() {
-    console.log(`Pagando com debit no valor de R$ ${this.amount}`);
+class EmailNotification {
+  send(message) {
+    console.log(`Enviando ${message}`);
   }
 }
 
-class CreditCard {
-  constructor(amount) {
-    this.amount = amount; // o amount fica armazenado na instância
-  }
-
-  pay() {
-    console.log(`Pagando com credit no valor de R$ ${this.amount}`);
+class SMSNotification {
+  send(message) {
+    console.log(`Enviando ${message}`);
   }
 }
 
-// Factory Method
-class ExpensesFactory {
+class PushNotification {
+  send(message) {
+    console.log(`Enviando ${message}`);
+  }
+}
+
+class WhatsAppNotification {
+  send(message) {
+    console.log(`Enviando ${message}`);
+  }
+}
+
+// Usando Factory Method
+class NotificationFactory {
   static types = {
-    credit: CreditCard,
-    debit: DebitCard,
+    email: EmailNotification,
+    sms: SMSNotification,
+    push: PushNotification,
+    whatsapp: WhatsAppNotification,
   };
 
-  static createExpenses(type, amount) {
-    const ExpensesClass = this.types[type];
-    if (!ExpensesClass) {
-      throw new Error("Tipo de gasto não suportado.");
+  static createNotification(type) {
+    const NotificationClass = this.types[type];
+    if (!NotificationClass) {
+      throw new Error("Tipo de notificação ainda não suportado");
     }
-    return new ExpensesClass(amount); // passa amount aqui
+    return new NotificationClass();
   }
 }
 
 // Código do cliente
 function main() {
-  const expensesTypes = ["credit", "debit"];
-  const amounts = { credit: 500, debit: 200 }; // valores de exemplo
-
+  const notifications = ["email", "sms", "push", "whatsapp"];
   try {
-    expensesTypes.forEach((type) => {
-      // Aqui pegamos o valor do objeto amounts
-      const amount = amounts[type];
-      // Criamos a instância já com o amount
-      const expenses = ExpensesFactory.createExpenses(type, amount);
-      // Chamamos pay sem passar parâmetro
-      expenses.pay();
+    notifications.forEach((type) => {
+      const notification = NotificationFactory.createNotification(type);
+      notification.send(
+        `${type}... Pix recebido de Ugioni no valor de R$ 100,00`
+      );
     });
   } catch (err) {
-    console.error("Erro ao processar gasto...", err.message);
+    console.error("Erro ao enviar...", err.message);
   }
 }
 
